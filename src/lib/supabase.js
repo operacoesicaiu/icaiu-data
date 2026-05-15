c1e0b1f452e3d92534cb95678762e132d245df29
𝓟aͥtriͣcͫ𝒌 <patrickajm@gmail.com>
2026-05-15 10:47:47 -0300

    fix actions

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,8 +1,12 @@
 const { createClient } = require('@supabase/supabase-js');
 
+
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY,
+  {
+    realtime: { enabled: false }
+  }
 );
 
 module.exports = supabase;
